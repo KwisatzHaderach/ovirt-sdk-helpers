e5d87cfa9e56b5470f55ba97aa0f248832b02f95
pmatyas <pmatyas@redhat.com>
2018-04-12 17:45:42 +0200

    Add global connection to be accessible from low/high level

---
+++ ovirt_sdk_helpers/connection.py
@@ -0,0 +1,44 @@
+import atexit
+import logging
+
+import ovirtsdk4
+
+from . import config
+
+_CON = None
+
+logging.basicConfig(level=logging.DEBUG, filename='base.log')
+logger = logging.getLogger(__name__)
+
+
+def create(
+    url=config.ENGINE_API_URL,
+    username=config.USERNAME,
+    password=config.PASSWORD
+):
+    """
+    Create a connection
+
+    Args:
+        url (str): ovirt engine url
+        username (str): username@domain to log in
+        password (str): password for the user to log in
+    """
+    global _CON
+    if _CON:
+        return
+    _CON = ovirtsdk4.Connection(
+        url=url,
+        username=username,
+        password=password,
+        insecure=True,
+        debug=True,
+        log=logger,
+    )
+
+
+def destroy():
+    global _CON
+    _CON.close()
+
+atexit.register(destroy)
--- ovirt_sdk_helpers/tests/test_connection.py
+++ ovirt_sdk_helpers/tests/test_connection.py
@@ -1,12 +1,9 @@
-from ovirt_sdk_helpers import base
-
-from .. import config
+from  ovirt_sdk_helpers import connection
 
 
 class TestConnection:
     def test_basic_connection(self):
-        c = base.get_connection(config.ENGINE_API_URL)
-        vms_service = c.system_service().vms_service()
+        connection.create()
+        vms_service = connection._CON.system_service().vms_service()
         for vm in vms_service.list():
             print(vm.name)
-        c.close()
